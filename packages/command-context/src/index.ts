40cb3b09686a045d8c72d28084d498a921c636b7
KChi <59391215+KagChi@users.noreply.github.com>
2022-05-01 16:08:34 +0700

    fix: export all module

--- packages/command-context/src/index.ts
+++ packages/command-context/src/index.ts
@@ -1,1 +1,6 @@
 export * from "./lib/ContextCommand";
+export * from "./lib/Structures/BaseCommandContext";
+export * from "./lib/Structures/BaseCommandInteractionCommandContext";
+export * from "./lib/Structures/CommandInteractionCommandContext";
+export * from "./lib/Structures/ContextMenuInteractionCommandContext";
+export * from "./lib/Structures/MessageCommandContext";
