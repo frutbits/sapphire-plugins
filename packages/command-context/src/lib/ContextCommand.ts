35692c132a81043c32f2aac46c1d5a4bdacbc32d
KChi <59391215+KagChi@users.noreply.github.com>
2022-05-01 15:44:12 +0700

    fix: add override

--- packages/command-context/src/lib/ContextCommand.ts
+++ packages/command-context/src/lib/ContextCommand.ts
@@ -12,15 +12,15 @@ export abstract class ContextCommand extends Command {
         super(context, options);
     }
 
-    public messageRun(message: Message, args: Args) {
+    public override messageRun(message: Message, args: Args) {
         return this.contextRun(new MessageCommandContext(message, args));
     }
 
-    public chatInputRun(interaction: CommandInteraction) {
+    public override chatInputRun(interaction: CommandInteraction) {
         return this.contextRun(new CommandInteractionCommandContext(interaction));
     }
 
-    public contextMenuRun(interaction: ContextMenuInteraction) {
+    public override contextMenuRun(interaction: ContextMenuInteraction) {
         return this.contextRun(new ContextMenuInteractionCommandContext(interaction));
     }
 
