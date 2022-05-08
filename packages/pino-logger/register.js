47049ec85a851baec4fa7e57c35fa0b18695cf86
KChi <59391215+KagChi@users.noreply.github.com>
2022-05-08 14:45:53 +0700

    fix: properly export register

---
+++ packages/pino-logger/register.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const tslib = require("tslib");
+
+tslib.__exportStar(require("./dist/register.js"), exports);
