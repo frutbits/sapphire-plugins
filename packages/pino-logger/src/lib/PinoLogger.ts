a4642ca8a8835936b22a21c537dce2cd2bee6fd8
Zen <45705890+ZenShibata@users.noreply.github.com>
2022-06-13 16:45:35 +0700

    fix(Pino): String message parameter (#32)
    * chore(deps): add pino-pretty
    
    * test: add error test
    
    * fix(PinoLogger): check if the first params is a string

--- packages/pino-logger/src/lib/PinoLogger.ts
+++ packages/pino-logger/src/lib/PinoLogger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 import type { ILogger } from "@sapphire/framework";
@@ -12,28 +13,46 @@ export class PinoLogger implements ILogger {
         this.pino = pino(options);
     }
 
-    public trace(...values: readonly [unknown]): void {
-        this.pino.trace(...values);
+    public trace(object: any, ...values: readonly [unknown]): void {
+        if (typeof object === "string") {
+            return this.pino.trace(...values, object);
+        }
+        return this.pino.trace(object, ...values);
     }
 
-    public debug(...values: readonly [unknown]): void {
-        this.pino.debug(...values);
+    public debug(object: any, ...values: readonly [unknown]): void {
+        if (typeof object === "string") {
+            return this.pino.debug(...values, object);
+        }
+        return this.pino.debug(object, ...values);
     }
 
-    public info(...values: readonly [unknown]): void {
-        this.pino.info(...values);
+    public info(object: any, ...values: readonly [unknown]): void {
+        if (typeof object === "string") {
+            return this.pino.info(...values, object);
+        }
+        return this.pino.info(object, ...values);
     }
 
-    public warn(...values: readonly [unknown]): void {
-        this.pino.warn(...values);
+    public warn(object: any, ...values: readonly [unknown]): void {
+        if (typeof object === "string") {
+            return this.pino.warn(...values, object);
+        }
+        return this.pino.warn(object, ...values);
     }
 
-    public error(...values: readonly [unknown]): void {
-        this.pino.error(...values);
+    public error(object: any, ...values: readonly [unknown]): void {
+        if (typeof object === "string") {
+            return this.pino.error(...values, object);
+        }
+        return this.pino.error(object, ...values);
     }
 
-    public fatal(...values: readonly [unknown]): void {
-        this.pino.fatal(...values);
+    public fatal(object: any, ...values: readonly [unknown]): void {
+        if (typeof object === "string") {
+            return this.pino.fatal(...values, object);
+        }
+        return this.pino.fatal(object, ...values);
     }
 
     public has(): boolean {
